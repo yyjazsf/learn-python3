076a839f188408e57e75e0bbaae090a492c9f2cf
yyjazsf <admin@yingyj.com>
2018-06-11 09:52:56 +0800

    use flask template

--- server_by_flask.py
+++ server_by_flask.py
@@ -1,31 +1,24 @@
-from flask import Flask, request
-import json
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/', methods=['GET'])
 def index():
-    with open('data/index.html', 'r') as f:
-        content = f.read()
-        return content
+    return render_template('index.html')
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'GET':
-        # need try
-        with open('data/login.html', 'r') as f:
-            content = f.read()
-            return content
+        return render_template('login.html')
     else:
         # 需要从request对象读取表单内容：
         form = request.form
         success = form['username'] == 'admin' and form['password'] == 'admin'
-        return str({
-            "success": str(success).lower(),
-            "username": request.form['username']
-        })
+        if success:
+            return render_template('index.html', message="登录成功", username=form['username'])
+        else:
+            return render_template('login.html', message="账号密码错误", username=form['username'])
 
 
 if __name__ == '__main__':
