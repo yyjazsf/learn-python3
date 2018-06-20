3541f7210eb78f1c2d9cc97b5cd16b3eb2e2ee4f
yyjazsf <admin@yingyj.com>
2018-06-20 18:21:52 +0800

    add route /

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,18 @@
 import logging
 from datetime import datetime
-import asyncio
 from flask import Flask, session, redirect, url_for, escape, request, jsonify
-
+from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 
 from .config import config
 
 conf = config.get_config()
 
-logging.basicConfig(level=logging.WARN)
+logging.basicConfig(level=conf['logging']['level'])
 
 app = Flask(__name__)
 
+login_manager = LoginManager()
 
 # can't use conf.database
 app.config['SQLALCHEMY_DATABASE_URI'] = conf['database']
