fcedb2e92d9ee2e553b1dfc5e8fd2e258a1d9a4d
yyjazsf <admin@yingyj.com>
2018-07-04 10:02:09 +0800

    修复数据

--- load12306.py
+++ load12306.py
@@ -10,26 +10,29 @@ sql = '''
 USE blog;
 DELETE FROM user;
 
+ALTER TABLE user AUTO_INCREMENT = 1;
+
 REPLACE INTO user (`email`, `username`, `real_name`, `card_number`, `password`, `telphone`, `create_time`)
 VALUES
 '''
-
+print('======开始解析')
 with open('data/12306.txt', 'r') as f:
     for line in f.readlines():
         # test /(\w+@\w+\.\w+).+(\w+@\w+\.\w+)[\r\n]/gm ?
         text = line.strip()
         text = re.sub(r'\\+', '', text)
         row = text.split('----')[: -1]
-
+        if len(row[5]) > 11:
+            row[5] = '00000000000'
         row.append(now)
-        row[1] = row[0]
         count += 1
 
         sql += r'''('%s', '%s', '%s', '%s', '%s', '%s', '%s'),
 ''' % tuple(row)
 
-        sql = sql[: -2] + ';'
+sql = sql[: -2] + ';'
 
+print('======开始写入')
 
 with open('data/12306.sql', 'w+') as f:
     f.write(sql)
